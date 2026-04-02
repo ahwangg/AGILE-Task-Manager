df934827de99bcff08e08af13032d53ba46a7728
Alex Hwang <alexhwang@Alexs-MacBook-Air-73.local>
2026-04-02 16:36:19 -0700

    updated user.py with password attribute and setter

--- backend/user.py
+++ backend/user.py
@@ -1,11 +1,12 @@
 #user.py
 
 class User:
-    def __init__(self, user_id: int, name: str, email: str):
+    def __init__(self, user_id: int, name: str, email: str, password: str):
         # User attributes
         self.userID = user_id
         self.name = name
         self.email = email
+        self.password = password
 
     # + update_name(updated_email: str): void
     def update_name(self, updated_name: str):
@@ -20,3 +21,10 @@ class User:
         Updates the users email
         '''
         self.email = updated_email
+
+    # + update_password(updated_password: str): void
+    def update_password(self, updated_password: str):
+        '''
+        Updates the users password
+        '''
+        self.password = updated_password
